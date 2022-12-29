5cca9594c57340809f43c06d8c1d7e036d26c647
francostan <francostan98@hotmail.com>
2022-12-29 18:42:29 -0300

    navbar and image

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
     //quede en el minuto 8:00
     //https://www.youtube.com/watch?v=y3nE6F5T9jo&ab_channel=DailyWebCoding
-  return (
-    <nav>NavBar</nav>
-  )
-}
 
+
+ const links = [
+		{
+			text: "About me",
+			path: "/about",
+		},
+		{
+			text: "Article",
+			path: "/article",
+		},
+		{
+			text: "Projects",
+			path: "/projects",
+		},
+		{
+			text: "Contact",
+			path: "/contact",
+		},
+	];
+
+	return (
+		<nav className="flex justify-center items-center w-full ">
+			<div className=" text-zinc-400 flex gap-6 items-center shadow-md border border-zinc-700 rounded-full px-5 py-3 text-base bg-zinc-800">
+				{links.map(({ text, path }, index) => {
+					return (
+						<Link
+							key={index}
+							className="hover:text-yellow-300 cursor-pointer"
+							href={path}
+						>
+							{text}
+						</Link>
+					);
+				})}
+			</div>
+		</nav>
+	);
+}
 export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import NavBar from "../components/NavBar";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,25 @@ export default function Home() {
       </Head>
       <div className="bg-black h-screen">
         <div className="fixed inset-0 max-w-6xl mx-auto h-screen border-l border-r border-zinc-700 bg-primary p-24 text-white ">
-          <div className="max-w-6xl mx-auto relative px-24"> 
-          <NavBar/>
+          <div className="max-w-6xl mx-auto relative px-24 py-0">
+            <NavBar />
+
+            <div className="max-w-2xl flex flex-col gap-5 mt-20">
+              <div className="relative w-40 h-40 align-items-center">
+                <Image
+                  src="/yo.jpg"
+                  fill
+                  alt=""
+                  className="object-center object-cover rounded-full"
+                />
+              </div>
+
+              <h1 className="text-lg font-semibold-sans tracking-tight text-yellow-300 sm:text-5xl">
+                Software Developer and part time Analogic Photographer.
+              </h1>
+
+            </div>
+
           </div>
 
         </div>
