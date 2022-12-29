21241ef4e9d095b873b110f91c19b2d12aaa6285
francostan <francostan98@hotmail.com>
2022-12-29 13:04:32 -0300

    cambio a tailwind

---
+++ components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    //quede en el minuto 8:00
+    //https://www.youtube.com/watch?v=y3nE6F5T9jo&ab_channel=DailyWebCoding
+  return (
+    <nav>NavBar</nav>
+  )
+}
+
+export default NavBar
